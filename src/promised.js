b31a189bc113bb7f6ce13c0876d35ee9eab2796f
Nat Pryce <sw@natpryce.com>
2015-10-21 16:12:54 +0100

    converted Promised to an ES6 class

--- src/promised.js
+++ src/promised.js
@@ -2,34 +2,35 @@
 var React = require('react');
 var R = require('ramda');
 
-function Promised(Decorated) {
-	return React.createClass({
-		displayName: "Promised",
-		
-		getInitialState() {
-			return {loading: true, error: null, value: null};
-		},
-		
-		render() {
-			if (this.state.loading) {
-				return <span>Loading...</span>;
-			}
-			else if (this.state.error !== null) {
-				return <span>Error: {this.state.error.message}</span>;
-			}
-			else {
-				var propsWithoutThePromise = R.dissoc('promise', this.props);
-				return <Decorated {...propsWithoutThePromise} {...this.state.value}/>;
-			}
-		},
-		
-		componentDidMount() {
-			this.props.promise.then(
-				(value) => this.setState({loading: false, value: value}),
-				(error) => this.setState({loading: false, error: error}));
+var Promised = Decorated => class Promised extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+			error: null,
+			value: null
+		};
+	}
+	
+	render() {
+		if (this.state.loading) {
+			return <span>Loading...</span>;
 		}
-	});
-}
+		else if (this.state.error !== null) {
+			return <span>Error: {this.state.error.message}</span>;
+		}
+		else {
+			var propsWithoutThePromise = R.dissoc('promise', this.props);
+			return <Decorated {...propsWithoutThePromise} {...this.state.value}/>;
+		}
+	}
+	
+	componentDidMount() {
+		this.props.promise.then(
+			value => this.setState({loading: false, value: value}),
+			error => this.setState({loading: false, error: error}));
+	}
+};
 
 
 module.exports = Promised;
