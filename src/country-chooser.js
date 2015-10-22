f0081e3db2d9f4bc5c07553b8aad6504fda74b78
Nat Pryce <sw@natpryce.com>
2015-10-22 12:21:05 +0100

    tidy

--- src/country-chooser.js
+++ src/country-chooser.js
@@ -5,7 +5,7 @@ var Country = require('./country');
 var CountryChooser = ({countries, onSelect}) =>
     <ul className="country-chooser">
     { 
-	countries.map(c => 
+        countries.map(c => 
             <li key={c.iso} onClick={() => onSelect(c.iso)}>
                 <Country {...c}/>
             </li>)
