0a469ba5e2fbbe281786a6270a562551e421ee12
Nat Pryce <sw@natpryce.com>
2015-10-28 10:22:35 +0000

    make the higher-order component a curried function so thatit can be used as an ES.next decorator

--- src/promised.js
+++ src/promised.js
@@ -2,45 +2,31 @@
 var React = require('react');
 var R = require('ramda');
 
-function Promised() {
-    var promiseProp;
-    var Decorated;
-    
-    if (arguments.length ==1) {
-        promiseProp = "promise";
-        Decorated = arguments[0];
-    }
-    else {
-        promiseProp = arguments[0];
-        Decorated = arguments[1];
+var Promised = R.curry((promiseProp, Decorated) => class extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {loading: true, error: null, value: null};
     }
     
-    return class extends React.Component {
-        constructor(props) {
-	    super(props);
-	    this.state = {loading: true, error: null, value: null};
-        }
-        
-        render() {
-	    if (this.state.loading) {
-	        return <span>Loading...</span>;
-	    }
-	    else if (this.state.error !== null) {
-	        return <span>Error: {this.state.error.message}</span>;
-	    }
-	    else {
-	        var propsWithoutThePromise = R.dissoc(promiseProp, this.props);
-	        return <Decorated {...propsWithoutThePromise} {...this.state.value}/>;
-	    }
-        }
-        
-        componentDidMount() {
-	    this.props[promiseProp].then(
-	        value => this.setState({loading: false, value: value}),
-	        error => this.setState({loading: false, error: error}));
-        }
+    render() {
+	if (this.state.loading) {
+	    return <span>Loading...</span>;
+	}
+	else if (this.state.error !== null) {
+	    return <span>Error: {this.state.error.message}</span>;
+	}
+	else {
+	    var propsWithoutThePromise = R.dissoc(promiseProp, this.props);
+	    return <Decorated {...propsWithoutThePromise} {...this.state.value}/>;
+	}
     }
-};
+    
+    componentDidMount() {
+	this.props[promiseProp].then(
+	    value => this.setState({loading: false, value: value}),
+	    error => this.setState({loading: false, error: error}));
+    }
+});
 
 
 module.exports = Promised;
